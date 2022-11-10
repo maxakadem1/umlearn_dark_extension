d9e69a4530e7e38439665cab9d27398138f5486f
Denys <80554676+VestHaider@users.noreply.github.com>
2022-11-09 20:55:24 -0600

    Update darkTheme.js

--- darkTheme.js
+++ darkTheme.js
@@ -1,7 +1,22 @@
 // class responsible for text color: .d2l-typography
 // class responsible for navigation link text color: d2l-navigation-s-item
 
-function changeToDarkTheme()
-{
+let media = document.querySelectorAll("img, picture, video");
 
-}
+
+//if button on
+document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
+
+media.forEach((mediaItem)=>
+{
+    mediaItem.style.filter = "invert(1) hue-rotate(180deg)";
+})
+
+//if button off
+
+document.querySelector("html").style.filter = "invert(0) hue-rotate(0)";
+
+media.forEach((mediaItem)=>
+{
+    mediaItem.style.filter = "invert(0) hue-rotate(0)";
+})
