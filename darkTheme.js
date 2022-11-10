ab17a4b38669e78a25392eaef654ab66ad073f06
Denys <80554676+VestHaider@users.noreply.github.com>
2022-11-09 20:31:39 -0600

    1

--- darkTheme.js
+++ darkTheme.js
@@ -1,5 +1,5 @@
 // class responsible for text color: .d2l-typography
-//class responsible for navigation link text color: d2l-navigation-s-item
+// class responsible for navigation link text color: d2l-navigation-s-item
 
 function changeToDarkTheme()
 {
