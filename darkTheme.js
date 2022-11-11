7cce6e2ce2c9f4256394ade52d615d3feadf7119
Denys <80554676+VestHaider@users.noreply.github.com>
2022-11-11 15:16:05 -0600

    Update darkTheme.js

--- darkTheme.js
+++ darkTheme.js
@@ -2,21 +2,38 @@
 // class responsible for navigation link text color: d2l-navigation-s-item
 
 let media = document.querySelectorAll("img, picture, video");
+const button = document.querySelector("button");
+const circle = document.querySelector(".circle");
+
+let buttonOn = false;
+
+button.addEventListener("click", () => 
+{
+  if (!buttonOn) {
+    buttonOn = true;
+    document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
+
+    media.forEach((mediaItem)=>
+    {
+        mediaItem.style.filter = "invert(1) hue-rotate(180deg)";
+    })
+  } 
+  else 
+  {
+    buttonOn = false;
+    document.querySelector("html").style.filter = "invert(0) hue-rotate(0)";
+
+    media.forEach((mediaItem)=>
+    {
+        mediaItem.style.filter = "invert(0) hue-rotate(0)";
+    })
+  }
+});
+
 
 
 //if button on
-document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
 
-media.forEach((mediaItem)=>
-{
-    mediaItem.style.filter = "invert(1) hue-rotate(180deg)";
-})
 
 //if button off
 
-document.querySelector("html").style.filter = "invert(0) hue-rotate(0)";
-
-media.forEach((mediaItem)=>
-{
-    mediaItem.style.filter = "invert(0) hue-rotate(0)";
-})
