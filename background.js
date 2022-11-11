2101ae1cb4d5f21174d6b76bc4ca0296b571c7be
Denys <80554676+VestHaider@users.noreply.github.com>
2022-11-11 16:29:33 -0600

    1

--- background.js
+++ background.js
@@ -1,54 +1,13 @@
 /*
-    triggers when the browser has starts loading a webpage.
+    triggers when the browser finished loading a webpage.
 */
 
-chrome.webNavigation.onCommitted.addListener(function (tab) 
+chrome.webNavigation.onCompleted.addListener(function () 
 {
-    // Prevents script from running when other frames load
-    if (tab.frameId == 0) 
+    if(chrome.storage.sync.get({buttonOn}))
     {
-        chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => 
-        {
-            // Get the URL of the webpage
-            let url = tabs[0].url;
-            // Remove unnecessary protocol definitions and www subdomain from the URL
-            let parsedUrl = url.replace("https://", "")
-                .replace("http://", "")
-                .replace("www.", "")
-
-            // Remove path and queries e.g. https://universityofmanitoba.desire2learn.com/login or https://universityofmanitoba.desire2learn.com?query=value
-            // We only want the base domain
-            let domain = parsedUrl.slice(0, parsedUrl.indexOf('/') == -1 ? parsedUrl.length : parsedUrl.indexOf('/'))
-                .slice(0, parsedUrl.indexOf('?') == -1 ? parsedUrl.length : parsedUrl.indexOf('?'));
-
-            try 
-            {
-                if (domain.length < 1 || domain === null || domain === undefined) 
-                {
-                    return;
-                }
-
-                else if (domain == "universityofmanitoba.desire2learn.com") 
-                {
-                    runDarkTheme();
-                    return;
-                }
-            } 
-
-            catch (err) 
-            {
-                throw err;
-            }
-
-        });
+        chrome.tabs.executeScript({
+            file: "appOn.js",
+            });
     }
 });
-
-function runDarkTheme() 
-{
-    // Inject script from file into the webpage
-    chrome.tabs.executeScript({
-        file: 'darkTheme.js'
-    });
-    return true;
-}
