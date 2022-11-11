ffe85d2f11a19dc99f66e06d732d48f4d024775c
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2022-11-11 15:56:57 -0600

    Lightbulb updated

--- darkTheme.js
+++ darkTheme.js
@@ -1,33 +1,26 @@
 // class responsible for text color: .d2l-typography
 // class responsible for navigation link text color: d2l-navigation-s-item
-if(document.querySelector(".popup"))
-{
-    
-    const button = document.querySelector(".button");
+if (document.querySelector(".popup")) {
+  const button = document.querySelector(".button");
+  const images = document.querySelector(".images");
 
-    let buttonOn = false;
+  let buttonOn = false;
 
-    button.addEventListener("click", () => 
-    {
-        if (!buttonOn) 
-        {
-            buttonOn = true;
-            chrome.tabs.executeScript(
-            {
-                file: "appOn.js"
-            })
-            
-        } 
-        else 
-        {
-            buttonOn = false;
-            chrome.tabs.executeScript(
-            {
-                file: "appOff.js"
-            })
-        }
-    });
+  button.addEventListener("click", () => {
+    if (!buttonOn) {
+      buttonOn = true;
+      chrome.tabs.executeScript({
+        file: "appOn.js",
+      });
+      //change 1.svg to 2.svg
+      images.src = "1.svg";
+    } else {
+      buttonOn = false;
+      chrome.tabs.executeScript({
+        file: "appOff.js",
+      });
+      //change 1.svg to 2.svg
+      images.src = "2.svg";
+    }
+  });
 }
-
-
-
