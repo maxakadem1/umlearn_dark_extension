b6010daba8526ed5134d0d4a814071914384a5a2
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2022-11-11 17:02:43 -0600

    small updates

--- appOff.js
+++ appOff.js
@@ -1,9 +1,10 @@
-(function(){
-    let media = document.querySelectorAll("img, picture, video");
-    document.querySelector("html").style.filter = "invert(0) hue-rotate(0)";
+(function () {
+  let media = document.querySelectorAll(
+    "img, picture, video, div.d2l-enrollment-card-image-container, slot, div.d2l-card-header"
+  );
+  document.querySelector("html").style.filter = "invert(0) hue-rotate(0)";
 
-    media.forEach((mediaItem)=>
-    {
-        mediaItem.style.filter = "invert(0) hue-rotate(0)";
-    })
+  media.forEach((mediaItem) => {
+    mediaItem.style.filter = "invert(0) hue-rotate(0)";
+  });
 })();
