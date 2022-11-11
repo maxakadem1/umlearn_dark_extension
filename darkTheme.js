b7544631f6c4033cbbb4574bbeff0643b9c7bf13
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2022-11-11 15:36:03 -0600

    Revert "some changes"
    This reverts commit dce8f1f030503ef8283ae591127fa3eb968e9207.

--- darkTheme.js
+++ darkTheme.js
@@ -1,6 +1,5 @@
 // class responsible for text color: .d2l-typography
 // class responsible for navigation link text color: d2l-navigation-s-item
-<<<<<<< Updated upstream
 if(document.querySelector(".popup"))
 {
     
@@ -26,38 +25,8 @@ if(document.querySelector(".popup"))
             file: "appOff.js"
         })
     }
-=======
-
-let media = document.querySelectorAll("img, picture, video");
-const button = document.querySelector("button");
-
-let buttonOn = false;
-
-button.addEventListener("click", () => {
-  if (!buttonOn) {
-    buttonOn = true;
-    document.querySelector("html").style.filter =
-      "invert(1) hue-rotate(180deg)";
-
-    media.forEach((mediaItem) => {
-      mediaItem.style.filter = "invert(1) hue-rotate(180deg)";
->>>>>>> Stashed changes
     });
+}
 
-    // change 1.svg to 2.svg in popup.html
-    document.querySelector("img").src = "2.svg";
-  } else {
-    buttonOn = false;
-    document.querySelector("html").style.filter = "invert(0) hue-rotate(0)";
 
-    media.forEach((mediaItem) => {
-      mediaItem.style.filter = "invert(0) hue-rotate(0)";
-    });
-    // change 2.svg to 1.svg in popup.html
-    document.querySelector("img").src = "1.svg";
-  }
-});
 
-//if button on
-
-//if button off
