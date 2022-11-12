435d7378083cd01315088ccabaa808febe6530a3
Denys <80554676+VestHaider@users.noreply.github.com>
2022-11-11 23:25:30 -0600

    Update appOff.js

--- appOff.js
+++ appOff.js
@@ -1,10 +1,25 @@
 (function () {
-  let media = document.querySelectorAll(
-    "img, picture, video, div.d2l-enrollment-card-image-container, slot, div.d2l-card-header"
-  );
-  document.querySelector("html").style.filter = "invert(0) hue-rotate(0)";
-
-  media.forEach((mediaItem) => {
-    mediaItem.style.filter = "invert(0) hue-rotate(0)";
-  });
-})();
+    let media = document.querySelectorAll(
+      "img, picture, video, .d2l-enrollment-card-image-container"
+    );
+    //find all d2l enrollment cards udner multiple shadow roots
+    let enrollmentCards = document.querySelector("d2l-my-courses")
+      .shadowRoot.querySelector("d2l-my-courses-container")
+      .shadowRoot.querySelector("#panel-search-my-enrollments > d2l-my-courses-content")
+      .shadowRoot.querySelector("d2l-my-courses-card-grid")
+      .shadowRoot.querySelectorAll("d2l-enrollment-card");
+  
+    document.querySelector("html").style.filter = "invert(0) hue-rotate(0)";
+  
+    media.forEach((mediaItem) => {
+      mediaItem.style.filter = "invert(0) hue-rotate(0)";
+    });
+  
+    //go over all enrollmentCards
+    enrollmentCards.forEach((enrollmentCard)=>
+    {
+        // get an image container under multiple nested shadow roots and change its style
+        enrollmentCard.shadowRoot.querySelector(".d2l-enrollment-card-image-container").style.filter = "invert(0) hue-rotate(0)";
+    })
+  })();
+  
