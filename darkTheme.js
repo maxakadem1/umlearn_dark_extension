7afeed8e570bfd0a9994b5bae61706ed9188693e
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2022-11-11 23:40:08 -0600

    some changes

--- darkTheme.js
+++ darkTheme.js
@@ -1,45 +1,46 @@
 if (document.querySelector(".popup")) {
-    const button = document.querySelector(".button");
-    const images = document.querySelector(".images");
+  const button = document.querySelector(".button");
+  const images = document.querySelector(".images");
 
-    let buttonOn = false;
-        
-    button.addEventListener("click", () => {
-        if (localStorage.getItem("buttonOn") === "false") {
-            buttonOn = true;
-            //change local storage to true
-            localStorage.setItem("buttonOn", "true");
+  let buttonOn = false;
 
-            chrome.tabs.query({ active: true }, function (tabs) {
-                let tab = tabs[0];
-                chrome.scripting.executeScript(
-                    {
-                        target: {tabId: tab.id},
-                        files: ['appOn.js']
-                    },
-                );
-            });
+  button.addEventListener("click", () => {
+    if (buttonOn === false) {
+      buttonOn = true;
+      //change local storage to true
+      chrome.storage.local.set({ buttonOn: true }, function () {
+        console.log("Value is set to " + buttonOn);
+      });
+      // localStorage.setItem("buttonOn", "true");
 
-        //change 1.svg to 2.svg
-        images.src = "1.svg";
-        } else {
-        buttonOn = false;
-        //change local storage to false
-        localStorage.setItem("buttonOn", "false");
-
-        chrome.tabs.query({ active: true }, function (tabs) {
-            let tab = tabs[0];
-            chrome.scripting.executeScript(
-                {
-                    target: {tabId: tab.id},
-                    files: ['appOff.js']
-                },
-            );
+      chrome.tabs.query({ active: true }, function (tabs) {
+        let tab = tabs[0];
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ["appOn.js"],
         });
+      });
 
-        //change 1.svg to 2.svg
-        images.src = "2.svg";
-        }
-    });
+      //change 1.svg to 2.svg
+      images.src = "1.svg";
+    } else {
+      buttonOn = false;
+      //change local storage to false
+      chrome.storage.local.set({ buttonOn: false }, function () {
+        console.log("Value is set to " + buttonOn);
+      });
+      //localStorage.setItem("buttonOn", "false");
+
+      chrome.tabs.query({ active: true }, function (tabs) {
+        let tab = tabs[0];
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ["appOff.js"],
+        });
+      });
+
+      //change 1.svg to 2.svg
+      images.src = "2.svg";
+    }
+  });
 }
-  
